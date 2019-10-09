2d4555612b87c8886a8eed40351b1046799d24fe
1147CR <cristinaromero002@gmail.com>
2019-10-08 21:37:53 -0500

    tarea

---
+++ TAREAS/1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argu[]){
+	char x;
+	printf("Escribe algo\n");
+	scanf("%c", &x);
+	
+	x: argu[1];
+	printf("%c/n",x);
+
+return 0;
+}
