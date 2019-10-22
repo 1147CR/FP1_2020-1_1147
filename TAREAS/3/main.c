d14d0ea994f627fc994192b954fcc0dcde3669da
1147CR <cristinaromero002@gmail.com>
2019-10-22 15:33:05 -0500

    Con comentarios

--- TAREAS/3/main.c
+++ TAREAS/3/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char*argv[]){
+	//Declarando variables
 	double x, y;
 
+	//Convirtiendo argumentos a flotantes
 	x= atof(argv[1]);
 	y= atof(argv[2]);
 
+	//Haciendo operaciones leyendo variables 
 	printf("%.2lf\n", x+y);
 	
 	printf("%.2lf\n", x-y);
--- TAREAS/4/main.c
+++ TAREAS/4/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main (int argc, char*argv[]){
+	
+	//Declarando variables y asignando valor
 	double x=0;
+
+	//Construyendo la estructura de repetición con for y convirtiendo argumentos a flotantes
 	for(int i=1; i<argc; i++){
 		double y=atof(argv[i]);
 		x=y+x;
 	}
+
+	//Haciendo la operación para generar promedio
 	double promedio=x/(argc-1);
 	printf("%.2lf\n", promedio);
 
