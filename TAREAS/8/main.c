bd28591914bdf23d644f875c99d8b8a4d43dcaeb
1147CR <54511346+1147CR@users.noreply.github.com>
2019-11-11 10:09:48 -0600

    Update main.c
    Correción

--- TAREAS/8/main.c
+++ TAREAS/8/main.c
@@ -2,26 +2,27 @@
 #include <stdlib.h>
 int main(int argc, char*argv[]){
 	//Declarando variables
-	float x, y, z;
+	double x, y, z;
 
 	//Convirtiendo argumentos, guardando en variable flotante
 	x=atof(argv[1]);
 	y=atof(argv[2]);
 
 	//Mientras x sea igual a 1 se convierte y a grados C
-	if(x!=0){
-		z=(y*1.8)+32;
-		//Mostrando el resultado
-		printf("%.2f\n", z);
-		return 0;
+if(x!=0){
+	z=(y-32)/1.8;
+			//Mostrando el resultado
+			printf("%.2lf\n", z);
+			return 0;
 	}
 	//Si no entonces cuando x valga 0 se convierte y a grados F
 	else
 		if(x==0){
-			z=(y-32)/1.8;
-			//Mostrando el resultado
-			printf("%.2f\n", z);
-			return 0;
+			z=(y*1.8)+32;
+		//Mostrando el resultado
+		printf("%.2lf\n", z);
+		return 0;
+
 		}
 
 	return 0;
