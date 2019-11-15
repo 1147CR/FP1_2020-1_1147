76e026f6e1847f9fb01dffe8d03be04ea643754e
1147CR <54511346+1147CR@users.noreply.github.com>
2019-11-15 00:03:16 -0600

    Comentarios

--- TAREAS/13/main.c
+++ TAREAS/13/main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char*argv[]){
+	//Declarando argumentos
 	int numeros[argc-1];
 	int i=0, j=1, numero;
-	while(i<argc-1){
+	while(i<argc-1){//Convirtiendo y guardando argumento
 		numero=atoi(argv[j]);
 		numeros[i]=numero;
 		i++;
@@ -11,14 +12,14 @@ int main(int argc, char*argv[]){
 	}
 	int temporal;
 	for(i=1; j<argc-1-i; j++){
-		for(j=0; j<argc-1-i;j++){
+		for(j=0; j<argc-1-i;j++){//Se acomodan los números
 			if(numeros[j]>numeros[j+1]){
 				temporal=numeros[j];
 				numeros[j]=numeros[j+1];
 				numeros[j+1]=temporal;
 			}
 		}
-	}
+	}//Imprimiendo el número acomodado
 	for(int i=0;i<argc-1;i++){
 		printf("%i\n", numeros[i]);
 	}
